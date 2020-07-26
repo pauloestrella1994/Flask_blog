7c133bb517e864916199468e103e07b4d8bda0ef
Paulo Netto <pauloestrella1994@gmail.com>
2020-07-25 23:21:54 -0300

    Adding the blogs posts

--- view_web.py
+++ view_web.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [{ 
+           'author': 'Paulo Netto',
+           'title': 'Blog post 1',
+           'content': 'This is my post 1',
+           'date_posted': 'June 24'
+
+         },
+        {  
+           'author': 'Paulo Netto',
+           'title': 'Blog post 1',
+           'content': 'This is my post 1',
+           'date_posted': 'June 24'
+}
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home_page.html')
+    return render_template('home_page.html', posts=posts)
 
 app.run(debug=True)
